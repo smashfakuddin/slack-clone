fd7eb7c6e1396a07a126b105d866ad2ea162f110
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-05-25 01:30:36 +0600

    added navbar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import Home from './Components/Home';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Switch>
+
+          <Route path="/">
+            <Header/>
+          </Route>
+        </Switch>
+      </>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3SOXBDhqJ3IHtFMPjRgt3wGS-WnfNi6g",
+    authDomain: "slack-clone-smashfakuddin.firebaseapp.com",
+    projectId: "slack-clone-smashfakuddin",
+    storageBucket: "slack-clone-smashfakuddin.appspot.com",
+    messagingSenderId: "83003159267",
+    appId: "1:83003159267:web:c710424267b00980014bd6"
+  };
